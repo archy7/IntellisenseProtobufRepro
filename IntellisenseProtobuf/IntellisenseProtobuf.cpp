eeb37d7c3ebc78386f3f154ea0306d2ff3f0c76f
abrichta <abrichta@ENG-ABRICHTA-LT>
2023-07-05 10:20:51 +0200

    Add project files.

---
+++ IntellisenseProtobuf/IntellisenseProtobuf.cpp
@@ -0,0 +1,12 @@
+﻿// IntellisenseProtobuf.cpp : Defines the entry point for the application.
+//
+
+#include "IntellisenseProtobuf.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
